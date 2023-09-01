e639402824f55baa9142d6c00ec0ffc9a8318146
LADES-MAC-04 <lades-mac-04@MacBook-Air-de-LADES-MAC-04.local>
2023-09-01 10:11:28 -0400

    initial commit

---
+++ src/main/java/merenda/com/demo/controle/alunoControle.java
@@ -0,0 +1,14 @@
+package merenda.com.demo.controle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class alunoControle {
+
+	@GetMapping("/")
+	public String index() {
+		return "/aluno/aluno-index";
+	}
+	
+}
