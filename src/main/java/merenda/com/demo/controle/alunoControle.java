6f1f6a7f4765ed5c870e4e9c9a5bcc3c2d1d5e6b
lades-mac-04 <lades-mac-04@10.3.34.0>
2023-09-05 14:33:34 -0400

    controle, model e html

--- src/main/java/merenda/com/demo/controle/alunoControle.java
+++ src/main/java/merenda/com/demo/controle/alunoControle.java
@@ -1,7 +1,10 @@
 package merenda.com.demo.controle;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import merenda.com.demo.modelo.Restricao;
 
 @Controller
 public class alunoControle {
@@ -10,7 +13,26 @@ public class alunoControle {
 	public String index() {
 		return "/aluno/aluno-index";
 	}
-	//tradutor
-	//sei la
 	
+	@GetMapping("/redirectRestricoes")
+	public String redirecionarRestricoes(Model model) {
+		Restricao restricao = new Restricao();
+		model.addAttribute("novaRestricao", restricao);
+		return "/aluno/redirectRestricoes";
+	}
+	
+	@GetMapping("/restricoes")
+	public String restricoes() {
+		
+		return "/aluno/restricoes";
+	}
+	
+	//@GetMapping("/novo")
+	//public String novoEstudante(Model model) {
+	//	Estudante estudante = new Estudante();
+	//	model.addAttribute("novoEstudante", estudante);
+	//	return "/novo-estudante";
+	//}
+	//tradutor
+	//sei la	
 }
