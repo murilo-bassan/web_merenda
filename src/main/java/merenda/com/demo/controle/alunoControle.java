36d6bad8208cdda15ad24957c6c824d90658d4f0
lades-mac-04 <lades-mac-04@10.3.34.0>
2023-09-06 11:23:35 -0400

    consigo nem explicar

--- src/main/java/merenda/com/demo/controle/alunoControle.java
+++ src/main/java/merenda/com/demo/controle/alunoControle.java
@@ -1,13 +1,25 @@
 package merenda.com.demo.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jakarta.validation.Valid;
 import merenda.com.demo.modelo.Restricao;
+//import merenda.com.demo.repositorio.RestricaoRepositorio;
+import merenda.com.demo.servico.RestricaoServico;
 
 @Controller
 public class alunoControle {
+	
+	
+	@Autowired
+	RestricaoServico restricaoServico;
 
 	@GetMapping("/")
 	public String index() {
@@ -21,18 +33,44 @@ public class alunoControle {
 		return "/aluno/redirectRestricoes";
 	}
 	
-	@GetMapping("/restricoes")
-	public String restricoes() {
-		
+	@GetMapping("/pagRestricoes")
+	public String restricoes(Model model) {
+		Restricao restricao = new Restricao();
+		model.addAttribute("novaRestricao", restricao);
 		return "/aluno/restricoes";
 	}
 	
-	//@GetMapping("/novo")
-	//public String novoEstudante(Model model) {
-	//	Estudante estudante = new Estudante();
-	//	model.addAttribute("novoEstudante", estudante);
-	//	return "/novo-estudante";
+	@PostMapping("/gravar")
+	public String gravarRestricao(@ModelAttribute("novaRestricao") @Valid Restricao restricao,
+			BindingResult erros, Model model, 
+			RedirectAttributes attributes) {
+		
+		if(erros.hasErrors()) {
+			return "/aluno/restricoes";
+		}
+		
+		restricaoServico.criarRestricao(restricao);
+		attributes.addFlashAttribute("mensagem", "Restricao salva com sucesso!");
+		return "redirect:/redirectRestricoes";
+	}
+	
+	//@PostMapping("/gravar")
+	//public String gravarEstudante(@ModelAttribute("novoEstudante") @Valid Estudante estudante,
+	//		BindingResult erros, Model model, 
+	//		RedirectAttributes attributes) {
+	//	
+	//	if(erros.hasErrors()) {
+	//		return "/novo-estudante";
+	//	}
+	//
+	//	Estudante estudante2 = estudanteRepositorio.findByLogin(estudante.getLogin());
+	//	if(estudante2 != null){
+	//		model.addAttribute("loginExiste", "Login já está cadastrado");
+	//		return "/novo-estudante";
+	//	}
+	//
+	//	estudanteServico.criarEstudante(estudante);
+	//	attributes.addFlashAttribute("mensagem", "Estudante salvo com sucesso!");
+	//	return "redirect:/novo";
 	//}
-	//tradutor
-	//sei la	
 }
