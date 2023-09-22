1cf5626f1299ae8a9b81fb55dfd0c3f289f23316
moomoo <murilogabassan@gmail.com>
2023-09-22 11:29:59 -0400

    formularios centralizados

--- src/main/java/merenda/com/demo/DemoApplication.java
+++ src/main/java/merenda/com/demo/DemoApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	
-	//pinto grande
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
