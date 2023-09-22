0d3f36c8b951a1414b965436a5372c84b7e2f7ad
moomoo <murilogabassan@gmail.com>
2023-09-22 09:44:26 -0400

    comentario teste

--- src/main/java/merenda/com/demo/DemoApplication.java
+++ src/main/java/merenda/com/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	//pinto grande
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
