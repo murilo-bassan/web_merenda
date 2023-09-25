cde351c2a09b4ae718e49e0b3ff817add1d4e8e9
pires-pedro <pedro.neto4@estudante.ifms.edu.br>
2023-09-25 11:42:34 -0400

    update noticias

--- src/main/java/merenda/com/demo/controle/NoticiaControle.java
+++ src/main/java/merenda/com/demo/controle/NoticiaControle.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -99,6 +99,7 @@ public class NoticiaControle {
 				}
 			}
 		}
+		
 		attributes.addFlashAttribute("mensagem", "Noticia adicionada com sucesso!");
 		return "redirect:/noticia/listar";
 	}
